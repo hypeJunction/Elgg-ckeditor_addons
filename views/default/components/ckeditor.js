90428ed3ff81d8146449cda9b07dae0231a01059
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-03-21 16:22:05 +0100

    feat(js): editor now responds to form reset event

--- views/default/components/ckeditor.js
+++ views/default/components/ckeditor.js
@@ -3,6 +3,14 @@ define(function (require) {
 	var elggCKEditor = require('elgg/ckeditor');
 	var $ = require('jquery');
 
+	$(document).on('reset', 'form', function () {
+		$(this).find('[data-cke-init]').each(function () {
+			if ($(this).data('ckeditorInstance')) {
+				$(this).ckeditorGet().setData('');
+			}
+		});
+	});
+
 	return function (selector) {
 		var selector = selector || '.elgg-input-longtext:not([data-cke-init])';
 		$(selector)
