0284f0f8ca445449d16fd9cd28059cdb43d18177
iionly <iionly@gmx.de>
2017-03-08 23:47:05 +0100

    fix(uploads): display existing images on browsing

--- views/default/object/ckeditor_file.php
+++ views/default/object/ckeditor_file.php
@@ -5,8 +5,17 @@ use hypeJunction\CKFile;
 $entity = elgg_extract('entity', $vars);
 /* @var $entity CKFile */
 
+$hash = md5(file_get_contents($entity->getFilenameOnFilestore()));
+switch ($entity->getMimeType()) {
+	case 'image/gif' :
+		$ext = 'gif';
+		break;
+	default :
+		$ext = 'jpg';
+}
+
 $img = elgg_format_element('img', array(
-	'src' => elgg_normalize_url("ckeditor/image/$entity->owner_guid/$entity->hash/$entity->ext"),
+	'src' => elgg_normalize_url("ckeditor/image/$entity->owner_guid/$hash/$ext"),
 	'class' => 'ckeditor-browser-image',
 	'data-callback' => elgg_extract('ckeditor_callback', $vars),
 	'width' => 100,
