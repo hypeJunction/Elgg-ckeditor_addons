eff1dbe9eee4acb514563912d863df7208c31c88
hectorespert <blackleg@users.noreply.github.com>
2017-08-14 17:42:58 +0200

    feat(translation): adds Spanish translation

---
+++ languages/es.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+	'ckeditor:settings:user' => 'Activado para usuarios',
+	'ckeditor:settings:admin' => 'Habilitar para administradores',
+	'ckeditor:basicstyles' => 'Estilos básicos',
+	'ckeditor:paragraph' => 'Párrafo',
+	'ckeditor:clipboard' => 'Portapapeles',
+	'ckeditor:editing' => 'Edición',
+	'ckeditor:links' => 'Enlaces',
+	'ckeditor:insert' => 'Insertar',
+	'ckeditor:styles' => 'Estilos',
+	'ckeditor:colors' => 'Colores',
+	'ckeditor:tools' => 'Herramientas',
+	'ckeditor:document' => 'Documento',
+	
+	'ckeditor:link:target:notset' => 'Por defecto',
+	'ckeditor:link:target:blank' => 'Nueva pestaña',
+	
+	'ckeditor:attributes:type:url' => 'URL',
+	'ckeditor:attributes:type:email' => 'Email',	
+);
+
